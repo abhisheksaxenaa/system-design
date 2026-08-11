4a5245b8e916d5fc9137c092f5267eabaa744c3c
Abhishek Saxena <absabhishek2012@gmail.com>
2026-08-11 22:00:22 +0530

    (#35) Adding operator condition to extend the feature

--- file-find-system/main.py
+++ file-find-system/main.py
@@ -9,19 +9,20 @@ def main():
     files.append(File(name="example.txt", size=10 * 1024, extension="txt")) # 10KB
     files.append(File(name="image.png", size=2 * 1024 * 1024, extension="png")) # 2MB
     files.append(File(name="Some Order.xml", size=2 * 1024, extension="xml")) # 2KB
+    files.append(File(name="Purchase Order.png", size=2 * 1024, extension="png")) # 2KB
     files.append(File(name="my_folder", size=0, extension="", is_directory=True))
 
     # Testing the ExtensionFilter
-    extension_filter = ExtensionFilter(extension="txt")
+    extension_filter = ExtensionFilter(extension="png")
     result = FileSearchEngine.search(files, extension_filter)
-    print(f"Files with .txt extension: {len(result)}")
+    print(f"Files with .png extension: {len(result)}")
     for file in result:
         print(file)
 
     # Testing the SizeFilter
-    size_filter = SizeFilter(min_size=10 * 1024, max_size=1 * 1024 * 1024)
+    size_filter = SizeFilter(min_size=10 * 1024, max_size=5 * 1024 * 1024)
     result = FileSearchEngine.search(files, size_filter)
-    print(f"\nFiles with size between 10KB and 1MB: {len(result)}")
+    print(f"\nFiles with size between 10KB and 5MB: {len(result)}")
     for file in result:
         print(file)
 
@@ -29,6 +30,13 @@ def main():
     name_filter = NameFilter(name="order")
     result = FileSearchEngine.search(files, name_filter)
     print(f"\nFiles with 'order' in the name: {len(result)}")
+    for file in result:
+        print(file)
+
+    # Testing combined filters using logical operators
+    combined_filter = (extension_filter & size_filter | name_filter)
+    result = FileSearchEngine.search(files, combined_filter)
+    print(f"\nFiles matching combined filter: {len(result)}")
     for file in result:
         print(file)
 
